b7be50b44ff3748122b752dfdead9eab5713c423
chenlu <630001384@qq.com>
2016-11-27 23:42:07 +0800

    fixbug

--- CountyCode/CLCountyCodeViewController.m
+++ CountyCode/CLCountyCodeViewController.m
@@ -125,6 +125,12 @@
    
 
 }
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    PlaceModel * model = [self.dataSource[indexPath.section] objectAtIndex:indexPath.row];
+    if (_countryCodeBlock) {
+        _countryCodeBlock([NSString stringWithFormat:@"+%@",model.code]);
+    }
+}
 
 - (void)configureCell:(CLCountryCodeTableViewCellTableViewCell *)cell
     forRowAtIndexPath:(NSIndexPath *)indexPath
