44e96eb13d791f87a3d5cd3e4a7bbf36f4c9ad8e
chenlu <630001384@qq.com>
2016-12-23 12:27:20 +0800

    fix

--- CountyCode/ViewController.m
+++ CountyCode/ViewController.m
@@ -37,8 +37,6 @@
         }];
         [_btn setTitle:code forState:UIControlStateNormal];
     };
-    //////
-    ////
 //    CLXibViewController *VC = [[CLXibViewController alloc] initWithNibName:@"CLXibViewController" bundle:nil];
     
     [self presentViewController:VC animated:YES completion:^{
