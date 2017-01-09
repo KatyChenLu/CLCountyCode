5cfebaf9e5e5a76787a7700efa5a6c0c436f8c16
chenlu <630001384@qq.com>
2017-01-09 14:58:57 +0800

    fix

--- CountyCode/ViewController.m
+++ CountyCode/ViewController.m
@@ -29,6 +29,7 @@
     _btn.tintColor =[UIColor whiteColor];
     [_btn setTitle:@"btn" forState:UIControlStateNormal];
     [_btn addTarget:self action:@selector(btnAction) forControlEvents:UIControlEventTouchUpInside];
+ 
     [self.view addSubview:_btn];
 }
 - (void)btnAction {
